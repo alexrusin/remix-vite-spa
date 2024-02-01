1c08ab6ddd34e36b56cda1cf7918a8d142f1312b
Alex Rusin <alexrusin_2000@yahoo.com>
2024-02-01 07:09:13 -0800

    initial commit

---
+++ app/routes/dashboard/$/_route.tsx
@@ -0,0 +1,42 @@
+import {
+  type ClientLoaderFunctionArgs,
+  Link,
+  useLoaderData,
+  json,
+} from "@remix-run/react";
+
+export async function clientLoader({ params }: ClientLoaderFunctionArgs) {
+  const filePath = params["*"];
+  return json({
+    filePath,
+  });
+}
+
+export default function Dashboard404() {
+  const { filePath } = useLoaderData<typeof clientLoader>();
+  return (
+    <>
+      <main className="grid min-h-full place-items-center bg-white px-6 py-24 sm:py-32 lg:px-8">
+        <div className="text-center">
+          <p className="text-base font-semibold text-indigo-600">Oops</p>
+          <h1 className="mt-4 text-3xl font-bold tracking-tight text-gray-900 sm:text-5xl">
+            Page unavailable
+          </h1>
+          <p className="mt-6 text-base leading-7 text-gray-600">
+            Sorry, this page{" "}
+            <span className="text-blue-400">/dashboard/{filePath}</span> is
+            unavailable
+          </p>
+          <div className="mt-10 flex items-center justify-center gap-x-6">
+            <Link
+              to="/dashboard"
+              className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+            >
+              Dashboard home
+            </Link>
+          </div>
+        </div>
+      </main>
+    </>
+  );
+}
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,19 @@
 import { unstable_vitePlugin as remix } from "@remix-run/dev";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import { flatRoutes } from "remix-flat-routes";
 
 export default defineConfig({
-  plugins: [remix({ unstable_ssr: false }), tsconfigPaths()],
+  plugins: [
+    remix({
+      unstable_ssr: false,
+      ignoredRouteFiles: ["**/.*"],
+      routes: async (defineRoutes) => {
+        return {
+          ...flatRoutes("routes", defineRoutes),
+        };
+      },
+    }),
+    tsconfigPaths(),
+  ],
 });
